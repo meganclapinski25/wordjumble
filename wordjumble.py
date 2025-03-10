0d0acf34a7e9996582df02af51fe0edb5529bbbf
Megan Clapinski <142531799+meganclapinski25@users.noreply.github.com>
2025-03-09 21:58:22 -0700

    starting again

--- wordjumble.py
+++ wordjumble.py
@@ -1,24 +1,22 @@
 import random 
 
+
 #Hash Table (Dictionary)
-word_dict = {
-    'tefon': "often",
-    'sokik': "kiosk",
-    'niumem': "immune",
-    "siconu": "cousin"
-}
+word_dict = [
+    "often",
+    "kiosk",
+    "immune",
+    "cousin"
+]
 
 # letter postiions for larger word
-postions = {
-    'often': [2,4],
-    'kiosk':[0,1,3],
-    "immune":[4],
-    "cousin":[3,4]
-}
 
-#Game Function 
-def play_game():
+secret_word = random.choice(word_dict)
+
+jumbled = list(secret_word)
     
-    for jumbled_word in word_dict.keys():
-        correct_word = word_dict[jumbled_word]
-        print(f"Unscrambled this word:{jumbled_word}")
+secret_word = random.choice(word_dict)
+jumbled = list(secret_word)
+random.shuffle(jumbled)
+
+print("Jumbled Word List: ", jumbled)
